b3f1206bc0cbce402340938216f080aae7ac18c7
Mahira Govender <ST10438866@vcconnect.edu.za>
2025-03-13 16:26:11 +0200

    Class - Rectangle

---
+++ ICE_3/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_3
+{
+    class Rectangle : Shape
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(string shape, double length, double width) : base(shape)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        public double ICalculateArea()
+        {
+            double Answer = Length * Width;
+            return Answer;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine(ICalculateArea() + "\n");
+        }
+    }
+}
