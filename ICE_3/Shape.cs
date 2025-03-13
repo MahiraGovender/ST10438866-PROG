8e5e601735455a3756ac0de58012a052d05a15aa
Mahira Govender <ST10438866@vcconnect.edu.za>
2025-03-13 16:24:57 +0200

    Abstract class - Shape

---
+++ ICE_3/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_3
+{
+    abstract class Shape
+    {
+        public string ShapeName { get; set; }
+
+        public Shape(string shapeName)
+        {
+            this.ShapeName = shapeName;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"{ShapeName}'s area is: ");
+        }
+    }
+}
